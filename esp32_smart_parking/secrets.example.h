2e850b9add8bf47dfaf56478f03bdeaada27b627
Resh <saabireshdk@gmail.com>
2026-06-14 18:23:09 +0800

    fixed: ESLint errors in Cloud Functions & changed git tracking

---
+++ esp32_smart_parking/secrets.example.h
@@ -0,0 +1,12 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// 1. WiFi Credentials
+#define WIFI_SSID "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+// 2. Firebase Credentials
+#define API_KEY "YOUR_FIREBASE_API_KEY"
+#define DATABASE_URL "YOUR_FIREBASE_DATABASE_URL"
+
+#endif
